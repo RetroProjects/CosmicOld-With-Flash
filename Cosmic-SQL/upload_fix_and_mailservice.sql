072fd8d72e329d7abe58ab1eed89944894e1b4aa
DevHabbos <developmenthabbo@gmail.com>
2022-09-19 16:10:42 -0300

    sending sql

---
+++ Cosmic-SQL/upload_fix_and_mailservice.sql
@@ -0,0 +1,3 @@
+INSERT INTO `website_settings` VALUES ('upload_path', '/var/www/cosmic.devraizer.nl/public/uploads/');
+INSERT INTO `website_settings` VALUES ('mailer_dsn', 'gmail://stackoverflow@gmail.com:admin123@default?verify_peer=0');
+INSERT INTO `website_settings` VALUES ('mailer_from', 'info@devraizer.nl');
